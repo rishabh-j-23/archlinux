f4d0517734853e072e34b8826611dcc74c5599a0
Rishabh Jain <rjain23112002@gmail.com>
2025-05-24 20:09:50 +0530

    config changes Sat May 24 08:09:50 PM IST 2025

---
+++ configs/hypr/scripts/bluetooth_ctl.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+
+# Declare options in an array
+options=(
+  "pon  power on bluetooth"
+  "pof  power off bluetooth"
+  "con  connect bluetooth devices"
+  "cona  connect all bluetooth devices"
+  "dis  disconnect bluetooth devices"
+  "gui  bluetooth settings gui"
+)
+
+# Show rofi-wayland menu
+choice=$(printf "%s\n" "${options[@]}" | rofi -dmenu -p "bluetooth")
+
+# Extract short code from selected option
+code=$(echo "$choice" | awk '{print $1}')
+
+# Match selection
+case "$code" in
+  *pon)
+    bluetoothctl power on
+    ;;
+  *pof)
+    bluetoothctl power off
+    ;;
+  *con)
+    bluetoothctl devices | rofi -dmenu -p "connect" | awk '{print $2}' | xargs -r bluetoothctl connect
+    ;;
+  *dis)
+    bluetoothctl devices | rofi -dmenu -p "disconnect" | awk '{print $2}' | xargs -r bluetoothctl disconnect
+    ;;
+  *gui)
+    blueman-manager &
+    ;;
+  *cona)
+    timeout 5 bash -c '
+      bluetoothctl devices | awk "{print \$2}" | while read -r mac; do
+        bluetoothctl connect "$mac"
+      done
+    '
+    ;;
+  *)
+    echo "No valid option selected"
+    ;;
+esac
